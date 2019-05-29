a0b560a66b99b347f004b7816ce322e9f93b2c23
Linus-Joker <milk790513@yahoo.com.tw>
2019-05-29 18:33:23 +0800

    updata email
    updata email

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,7 +67,7 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
-            'mail' => $data['email'],
+            'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
     }
