c6c15cdc45eb8e1054ac1770036bd682531c870e
Linus-Joker <milk790513@yahoo.com.tw>
2019-05-30 20:30:26 +0800

    deete pic and add blog db table
    deete pic and add blog db table

---
+++ database/migrations/2019_05_30_122702_create_blog_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('article_title', 100);
+            $table->string('article_description', 511);
+            $table->string('article_pic', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog');
+    }
+}
