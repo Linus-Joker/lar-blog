486994c1aac32a4a147d972ec09db16229f4cdc0
Linus-Joker <milk790513@yahoo.com.tw>
2019-06-02 15:14:30 +0800

    cancel id
    cancel id

--- database/migrations/2019_06_02_064817_create_blogclient_table.php
+++ database/migrations/2019_06_02_064817_create_blogclient_table.php
@@ -14,7 +14,7 @@ class CreateBlogclientTable extends Migration
     public function up()
     {
         Schema::create('blogclient', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            // $table->bigIncrements('id');
             $table->string('name', 100);
             $table->string('password', 255);
             $table->integer('phone', 21);
