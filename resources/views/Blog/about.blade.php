2567bc0e87ff2cf528e0b0b31432c32a343e4267
Linus-Joker <milk790513@yahoo.com.tw>
2019-06-12 16:52:39 +0800

    modify about space
    modify about space

--- resources/views/Blog/about.blade.php
+++ resources/views/Blog/about.blade.php
@@ -5,14 +5,14 @@
 <div class="container">
   <div class="row">
     <div class="col-md-8">
-      <h1 class="font-weight-bold mb-3">關於我</h1>
+      <h1 class="font-weight-bold mt-5">關於我</h1>
         <p>hi,我是Linus，這裡是我使用Laravel框架設計的部落格製作練習網站</p>
         <p>會記錄我學習的心得和筆記!!</p>
     </div>
 
     <aside class="col-md-4">
         <div>
-          <h3 class="font-weight-bold mb-3">最新文章</h3>
+          <h3 class="font-weight-bold mt-5">最新文章</h3>
           {{-- {{$items}} --}}
             <ul>
               @foreach ($key as $i)
