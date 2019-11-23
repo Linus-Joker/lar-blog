77b78f04f5e24b23496bb791b9cdb4a47592ff9d
Linus-Joker <milk790513@yahoo.com.tw>
2019-11-23 17:40:06 +0800

    login check & ga _code
    add login check and googla analytics code

--- resources/views/AdminLayouts/home.blade.php
+++ resources/views/AdminLayouts/home.blade.php
@@ -5,20 +5,37 @@
 <div style="width:300px; margin:0 auto;" class="pt-3">
   <form action="logins" method="post">
     {{ csrf_field() }}
-    
+
     <div class="form-group">
-      <label for="name">帳號:</label>  
+      <label for="name">帳號:</label>
       <input type="text" name="user_name" id="" placeholder="請輸入帳號">
     </div>
     <div class="form-group">
       <label for="password">密碼:</label>
       <input type="password" name="user_pass" id="" placeholder="請輸入密碼">
-    </div>  
-    <button type="submit">登入</button>
+    </div>
+    <button type="submit" onclick="return checkform()">登入</button>
+
+    {!! Captcha::display() !!}
   </form>
 
-  {!! Captcha::display() !!}
+
   <a href="{{url('registed')}}">加入會員</a>
 </div>
-    
+
+<script>
+    function checkform(){
+        chksubmit = false;
+        var response = grecaptcha.getResponse();
+        if(response.length == 0){
+            alert("請驗證!");
+            return chksubmit;
+        }else{
+            chksubmit = true;
+            return chksubmit;
+        }
+    }
+
+</script>
+
 @endsection
--- resources/views/Blog/blog.blade.php
+++ resources/views/Blog/blog.blade.php
@@ -9,36 +9,36 @@
         <h2>
           <a href="{{url('a/'. $item->article_id)}}" target="_blank">
             <b>{{$item->article_title}}</b>
-          </a> 
+          </a>
         </h2>
-        <figure>  
+        <figure>
         {{-- <img src="{{asset('images/2019050608.jpg')}}" alt="p1"> --}}
         <a href="{{url('a/'. $item->article_id)}}" target="_blank">
           <img src="{{url($item->article_pic)}}" alt="p1" style="max-width:100%;">
         </a>
           {{-- <img src="https://picsum.photos/600/300" alt="p1"> --}}
         </figure>
-        
+
         {{-- <p>{{date('Y-m-d',$item->created_at)}}</p> --}}
         <time>{{$item->created_at}}</time>
         <p>{!!$item->article_description!!}</p>
         <div class="text-right">
           <a href="{{url('a/'. $item->article_id)}}"      target="_blank" class="btn btn-outline-primary">
             Read more...
-          </a> 
+          </a>
         </div>
         <hr>
       @endforeach
       {{ $data->links() }}
     </div>
-  
+
     <aside class="col-md-4">
       <div>
         <h3 class="font-weight-bold mb-3">最新文章</h3>
         {{-- {{$items}} --}}
           <ul>
             @foreach ($items as $i)
-            <a href="{{url('a/'. $i->article_id)}}" target="_blank"> 
+            <a href="{{url('a/'. $i->article_id)}}" target="_blank">
               <li>{{$i->article_title}}</li>
             </a>
             <hr>
@@ -49,5 +49,17 @@
 
   </div>
 </div>
-    
+
+
+<script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-153154258-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-153154258-1');
+    </script>
+</script>
 @endsection
